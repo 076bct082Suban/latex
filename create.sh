e410ca0c0d616ca1f941bfc26d35be5abbf5a406
Suban <076bct082.suban@pcampus.edu.np>
2021-05-04 13:55:25 +0545

    First file copied

---
+++ create.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+CURPATH=`pwd`
+
+# argument structure
+# ./create {type} {docmuent name}
+
+if [ -z "$1" ]
+then
+    echo "please enter type of the file you wish to create"
+    exit 1
+fi
+if [ -z "$2" ]
+then 
+    echo "Please enter file name"
+    exit 1
+fi
+
+DIR="$CURPATH/template/$1"
+NAME="$2"
+OUTDIR="$3"
+
+if [ -d "$DIR" ]
+then
+    echo "Template found. "
+else
+    echo "Template Not found. "
+fi
+
+if  [ -z "$3" ]
+then
+    # If we don't have a third argument (location) outputs pwd
+    `cp -R $DIR ./$NAME`
+
+else
+
+    `cd $OUTDIR;cp -R $DIR ./$NAME`
+fi
+
+
+
